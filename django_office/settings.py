b810c90fb7dfa19ef5a41e80929b56601e7ee7a3
yuanjia123 <871105356@qq.com>
2020-10-15 15:16:32 +0800

    创建了一个pulls投票应用

--- django_office/settings.py
+++ django_office/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'django_office.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "django_office",
+        "USER":"root",
+        "PASSWORD":"123456",
+        "HOST":"127.0.0.1"
     }
 }
 
